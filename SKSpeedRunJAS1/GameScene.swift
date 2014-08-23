c35c69ab3009a887911f8f9f48f8fc1ef195d972
Jim Stewart <jim@stewartstuff.com>
2014-08-23 16:34:04 -0500

    After adding starfield and demo in Xcode beta 5

--- SKSpeedRunJAS1/GameScene.swift
+++ SKSpeedRunJAS1/GameScene.swift
@@ -10,36 +10,25 @@ import SpriteKit
 
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
+        self.backgroundColor = SKColor.blackColor()
+        self.setupStarfield()
     }
     
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
-        
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
-    }
-   
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
+    
+    func setupStarfield() {
+        let starfieldPath = NSBundle.mainBundle().pathForResource("Starfield", ofType: "sks")
+        let starfieldNode = NSKeyedUnarchiver.unarchiveObjectWithFile(starfieldPath!) as SKEmitterNode
+        starfieldNode.position = CGPointMake( CGRectGetMidX(self.frame), CGRectGetHeight(self.frame))
+        
+        starfieldNode.particleColorSequence = nil
+        starfieldNode.particleColor = SKColor.redColor()
+        starfieldNode.particleColorBlueRange = 255.0
+        starfieldNode.particleColorGreenRange = 255.0
+        
+        self.addChild(starfieldNode)
+    }
+
 }
